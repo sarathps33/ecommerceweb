6a697672a0c6ebb29b94e6e3f36996a25fff354a
sarathps33 <sarathps224@gmail.com>
2024-08-14 15:02:32 +0530

    add

---
+++ js/cart.js
@@ -0,0 +1,62 @@
+
+function displayCartItems(){
+    str=``
+    if(localStorage.length==0){
+    window.location.href='../index.html'
+
+    }
+    else{
+        for(i=0;i<localStorage.length;i++){
+            // console.log(localStorage);
+            const key =localStorage.key(i);
+            // console.log(key);
+       
+             const product = JSON.parse(localStorage.getItem(key));
+            // console.log(product);
+            str+=`
+            <div>
+                <div class="men-card">
+                    <div class="image">
+                        <img src="${product.thumbnail}" alt="">
+                    </div>
+                    <div class="content">
+                    
+                        <h3 class="id">${product.id}</h3>
+                        <h5 class="category">${product.category.toUpperCase()}</h5>
+                        <h4>${product.title.length>=20?`${product.title.substring(0,20)}...`:product.title}</h4>
+                        <div> <span><button class="ratings">${product.rating} <span><i class="fa-solid fa-star"></i></span></button></span></div>
+                        <span class="discount-price">$${product.price}</span>
+                        <span class="original-price">$${Math.ceil(product.price*100/product.discountPercentage)}</span>
+                        <span class="discount-percentage">${product.discountPercentage}% OFF</span>
+    
+                       
+                  
+                    </div>
+    
+                    <div class="content-buttons">
+                        <button class="remove-btn" onclick="removeItem(${key})">REMOVE</button>
+                       
+                    </div>
+              
+                </div>
+            </div>`
+    
+            
+    
+        }
+        document.getElementById("products").innerHTML=str
+    }
+
+}
+
+
+displayCartItems()
+
+function removeItem(key){
+          
+            localStorage.removeItem(key);
+            displayCartItems()
+    
+}
+
+// -------------------------
